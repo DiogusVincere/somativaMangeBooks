683390b32c4b47dcb5d32ba13e5e88f41e7d450a
DiogusVincere <diogovlima03@gmail.com>
2024-12-12 13:06:47 -0300

    config vercel 8

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -7,15 +7,15 @@ const api = axios.create({
 // Instância para rotas com '/api'
 export const apiWithPrefix = axios.create({
   baseURL: process.env.NODE_ENV === 'production'
-    ? 'https://backend-qw27yrwzw-diogusvinceres-projects.vercel.app/api/auth'
-    : 'http://localhost:5000/api/auth', // URL do backend em desenvolvimento
+    ? 'https://backend-qw27yrwzw-diogusvinceres-projects.vercel.app/api'
+    : 'http://localhost:5000/api', // URL do backend em desenvolvimento
 });
 
 // Instância para rotas sem '/api'
 export const apiWithoutPrefix = axios.create({
   baseURL: process.env.NODE_ENV === 'production'
     ? 'https://backend-qw27yrwzw-diogusvinceres-projects.vercel.app/books'
-    : 'http://localhost:5000/books', // URL do backend em desenvolvimento
+    : 'http://localhost:5000', // URL do backend em desenvolvimento
 });
 
 // Adicionando interceptor para incluir o token nas requisições
